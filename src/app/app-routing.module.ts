88c9b66dccdbb9be9f4d95a0066b25ad00ce3467
WebDeveloper244 <sb2137842@gmail.com>
2023-06-24 07:31:23 +0500

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './admin-component/dashboard/dashboard.component';
 import { AnalyticsComponent } from './admin-component/analytics/analytics.component';
+import { AngularComponent } from './admin-component/angular/angular.component';
+import { NodeComponent } from './admin-component/node/node.component';
+import { BootstrapComponent } from './admin-component/bootstrap/bootstrap.component';
 
 const routes: Routes = [
   {
@@ -15,6 +18,18 @@ const routes: Routes = [
   {
     path:"analytics", 
     component:AnalyticsComponent
+  },
+  {
+    path:"angular", 
+    component:AngularComponent
+  },
+  {
+    path:"node", 
+    component:NodeComponent
+  },
+  {
+    path:"bootstrap", 
+    component:BootstrapComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,19 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';;
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { AngularComponent } from './admin-component/angular/angular.component';
+import { NodeComponent } from './admin-component/node/node.component';
+import { BootstrapComponent } from './admin-component/bootstrap/bootstrap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     AnalyticsComponent,
-    SideBarComponent
+    SideBarComponent,
+    AngularComponent,
+    NodeComponent,
+    BootstrapComponent
   ],
   imports: [
     BrowserModule,
